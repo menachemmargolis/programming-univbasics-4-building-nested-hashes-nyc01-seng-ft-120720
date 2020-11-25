178572439be9a58ed09da6fb1f392e92356bc3a9
menachem m margolis <margolismendel5728@gmail.com>
2020-11-25 18:47:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity
- {:railroads=>{}}
- monopoly_with_second_tier[:railroades][:pieces]=4,
- monopoly_with_second_tier[:railroades][:rent_in_dollars]={},
+ monopoly_with_second_tier={:railroads=>{}}
+ monopoly_with_second_tier[:railroades]=[:pieces=>4]
+ monopoly_with_second_tier[:railroades]=[:rent_in_dollars=>{}]
 
- monopoly_with_second_tier[:railroades][:names]={}
+ monopoly_with_second_tier[:railroades]=[:names=>{}]
 
 end
 binding.pry
