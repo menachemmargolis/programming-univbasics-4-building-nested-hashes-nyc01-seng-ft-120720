cdb6313a5ac96a297fac48f2edc8be0215b545f6
menachem m margolis <margolismendel5728@gmail.com>
2020-11-25 19:03:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,8 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity
-  {:railroads=>{}}
-monopoly_with_second_tier[:railroads][:pieces]=4
-monopoly_with_second_tier[:railroads][:rent_in_dollars]={}
-monopoly_with_second_tier[:railroads][:names]={}
+ {:railroads=>{}}
+ monopoly_with_second_tier[:railroads][:pieces]=4
 end
 
 def monopoly_with_third_tier
@@ -20,10 +18,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {:railroads=>{}}
-monopoly_with_third_tier[:railroads][:pieces]=4
-monopoly_with_third_tier[:railroads][:rent_in_dollars]={}
-monopoly_with_third_tier[:railroads][:names]={}
+
 
 
 end
